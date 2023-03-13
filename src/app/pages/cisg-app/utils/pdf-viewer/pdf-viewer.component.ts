0f94fe09c13ded4c5cec2a086620f93e5c936220
ae.appjak <ae.appjak@gmail.com>
2023-03-12 20:28:34 -0600

    correcciones

--- src/app/pages/cisg-app/utils/pdf-viewer/pdf-viewer.component.ts
+++ src/app/pages/cisg-app/utils/pdf-viewer/pdf-viewer.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
+import {LoaderService} from "../loader.service";
 
 @Component({
   selector: 'ea-pdf-viewer',
@@ -9,7 +10,13 @@ import {ActivatedRoute} from "@angular/router";
 export class PdfViewerComponent {
   pdfSrc: string = "https://firebasestorage.googleapis.com/v0/b/centro-integral-sophie-germain.appspot.com/o/library";
 
-  constructor(private actRoute: ActivatedRoute) {
+  constructor(private actRoute: ActivatedRoute, private _loaderService: LoaderService) {
     this.pdfSrc = this.actRoute.snapshot.params['id']
+    _loaderService.loading = true
+  }
+
+  loadComplete() {
+    console.log('loadComplete')
+    this._loaderService.loading = false
   }
 }
